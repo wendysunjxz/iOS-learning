4da8e2bda88b4fdf15682e4c43ac6ec973c60228
Sun Wenyan <sunwy@Shuhaos-MacBook-Pro.local>
2015-03-06 19:30:37 -0800

    iOS Learning commit

---
+++ CH12 CarValet Starter/CarValet/CarDetailViewController.h
@@ -0,0 +1,24 @@
+//  CarDetailViewController.h
+//  CarValet
+
+#import <UIKit/UIKit.h>
+#import "ViewCarProtocol.h"
+
+@class CDCar;
+
+@interface CarDetailViewController : UIViewController
+<UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) CDCar *myCar;
+
+@property (weak, nonatomic) IBOutlet UITextField *carMakeField;
+@property (weak, nonatomic) IBOutlet UITextField *carModelField;
+@property (weak, nonatomic) IBOutlet UITextField *carYearField;
+@property (weak, nonatomic) IBOutlet UIImageView *carImageView;
+@property (weak, nonatomic) IBOutlet UILabel *dayParkedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeParkedLabel;
+@property (weak, nonatomic) IBOutlet UIPickerView *fuelPicker;
+
+@property (weak,nonatomic) id <ViewCarProtocol> delegate;
+
+@end
---
+++ CH12 CarValet Starter/CarValet/ReturnGestureRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  ReturnGestureRecognizer.h
+//  CarValet
+//
+//  Created by Sun Wenyan on 2015-03-01.
+//  Copyright (c) 2015 Learning iOS Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReturnGestureRecognizer : UIGestureRecognizer
+
+@end
